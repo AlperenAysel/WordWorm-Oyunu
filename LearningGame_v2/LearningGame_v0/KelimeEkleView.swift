a9c69e90413ab0f20a72537ffeb975c3f62b749b
AlperenAysel <48491546+AlperenAysel@users.noreply.github.com>
2019-03-16 20:12:32 +0300

    Add files via upload
    Egzersiz yap bölümde doğru cevabı bulabilen versiyon
    
    *Doğru cevap bulunduğunda database kullanarak öğrenme seviyesi artırılıcak

---
+++ LearningGame_v2/LearningGame_v0/KelimeEkleView.swift
@@ -0,0 +1,61 @@
+//
+//  KelimeEkleView.swift
+//  LearningGame_v0
+//
+//  Created by Alperen on 12.03.2019.
+//  Copyright © 2019 Alperen. All rights reserved.
+//
+
+import UIKit
+import SQLite
+
+class KelimeEkleView: UIViewController {
+
+    @IBOutlet weak var EkleButtonStyle: UIButton!
+    @IBOutlet weak var ingKelimeTextField: UITextField!
+    @IBOutlet weak var trKelimeEkleTextField: UITextField!
+    @IBOutlet weak var kelimeTuruTextField: UITextField!
+    
+    var db = DataBase()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        EkleButtonStyle.layer.cornerRadius = 25
+
+    }
+    
+    @IBAction func EkleTouch(_ sender: UIButton) {
+                
+
+        if (!ingKelimeTextField.text!.trimmingCharacters(in: .whitespaces).isEmpty && !trKelimeEkleTextField.text!.trimmingCharacters(in: .whitespaces).isEmpty && !kelimeTuruTextField.text!.trimmingCharacters(in: .whitespaces).isEmpty) {
+            
+            let eklenenTarih = Date()
+            
+            db.KelimeEkle(ingilizcesi: ingKelimeTextField.text!, turkcesi: trKelimeEkleTextField.text!, turu: kelimeTuruTextField.text!, tarihi: eklenenTarih)
+            
+                let doneAlertController = UIAlertController(title: "Başırılı", message: "Kelime Eklendi!", preferredStyle: .alert)
+                doneAlertController.addAction(UIAlertAction(title: "Tamam", style: UIAlertAction.Style.default, handler: {_ in }))
+                
+                self.present(doneAlertController, animated: true, completion: nil)
+
+                ingKelimeTextField.text = nil
+                trKelimeEkleTextField.text = nil
+                kelimeTuruTextField.text = nil
+                
+
+        } else {
+            let failAlertController = UIAlertController(title: "Kelime Eklenemedi", message: "Gerekli alanların hepsini doldurduğunuzdan emin olun.", preferredStyle: .alert)
+            failAlertController.addAction(UIAlertAction(title: "Tamam", style: UIAlertAction.Style.default, handler: {_ in }))
+            
+            self.present(failAlertController, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    @IBAction func Lİstele(_ sender: UIButton) {
+        db.ListeleTest()
+        }
+    }
+
---
+++ LearningGame_v2/LearningGame_v0/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LearningGame_v0
+//
+//  Created by Alperen on 12.03.2019.
+//  Copyright © 2019 Alperen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var KelimeEkleButton: UIButton!
+    @IBOutlet weak var TestEtButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        KelimeEkleButton.layer.cornerRadius = 25
+        TestEtButton.layer.cornerRadius = 25
+        
+    }
+
+
+}
+
+
+
